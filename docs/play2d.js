0c4457769bd7381fcfcef1e8d67a8c20ff436b7b
Keith Frost <keith.l.frost@gmail.com>
2025-08-23 23:27:30 -0400

    update play2d.js to work better

--- docs/play2d.js
+++ docs/play2d.js
@@ -7,23 +7,14 @@ const cssWidth = 640;
 const cssHeight = 640;
 canvas.style.width = cssWidth + 'px';
 canvas.style.height = cssHeight + 'px';
-const cWidth = cssWidth * pixelRatio;
-const cHeight = cssHeight * pixelRatio;
+const cWidth = Math.floor(cssWidth * pixelRatio);
+const cHeight = Math.floor(cssHeight * pixelRatio);
 canvas.width = cWidth;
 canvas.height = cHeight;
 ctx.scale(pixelRatio, pixelRatio);
 
 const imageData = ctx.createImageData(cWidth, cHeight);
 const imgData = imageData.data;   // Uint8ClampedArray
-for (let r = 0; r < cHeight; r++) {
-    for (let c = 0; c < cWidth; c++) {
-        index = (r * cWidth + c) * 4;
-        imgData[index] = r % 256;
-        imgData[index + 1] = c % 256;
-        imgData[index + 2] = index % 256;
-        imgData[index + 3] = 255;
-    }
-}
 
 let pTime = 0.0;
 let sumTime = 0.0;
@@ -42,15 +33,13 @@ function animate(time) {
         sumTime = 0.0;
         frames = 0;
     }
-    let t = Math.floor(deltaTime * 256);
-    t = t % 2048;
-    t += 2048;
+    const t = Math.floor(time * 256) % 2048;
     for (let r = 0; r < cHeight; r++) {
         for (let c = 0; c < cWidth; c++) {
             index = (r * cWidth + c) * 4;
-            imgData[index] = (imgData[index] + t) % 256;
-            imgData[index + 1] = (imgData[index + 1] + t) % 256;
-            imgData[index + 2] = (imgData[index + 2] + t) % 256;
+            imgData[index] = (r + t) % 256;
+            imgData[index + 1] = (c + t) % 256;
+            imgData[index + 2] = (index + t) % 256;
             imgData[index + 3] = 255;
         }
     }
